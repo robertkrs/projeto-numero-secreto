7e5bb988d848ee24e7520d1f954c8874e3a80d68
Robert Reis <robertreis323@gmail.com>
2022-12-21 20:40:17 -0300

    commit

--- app/reconhecimentoDeVoz.js
+++ app/reconhecimentoDeVoz.js
@@ -1,5 +1,20 @@
 window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
-
+const elementoChute = document.getElementById('chute');
 const recognition = new SpeechRecognition();
 recognition.lang = 'pt-br';
 recognition.start();
+
+recognition.addEventListener('result', onSpeak);
+
+function onSpeak(e){
+    const chute = e.results[0][0].transcript;
+
+    exibeChuteNaTela(chute);
+}
+
+function exibeChuteNaTela(chute){
+    elementoChute.innerHTML = `
+        <div>Você disse</div>
+        <span class='box'>${chute}</span>
+    `
+}
